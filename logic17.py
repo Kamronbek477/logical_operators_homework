510226816de32fcb9aae5456a74e2d9219b359f4
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-10-19 01:27:22 +0500

    logic17.py

--- logic17.py
+++ logic17.py
@@ -1,9 +1,10 @@
 def main(a):
-    """
-    Given a five-digit integer a,  check the following statement "All digits of the number are in ascending order".
+    """Given a five-digit integer a,  check the following statement "All digits of the number are in ascending order".
     Args:
         a(int): parameter a
     Returns:
         bool: answer
     """
-    return
+    
+    return 10000<=a<100000 and (a%10)<(a%100//10)<(a%1000//100)<(a%10000//1000)<(a//10000)
+print(main(97541))
