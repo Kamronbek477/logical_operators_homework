25a338132c395a4b19d5d1a3f05d6e55bbefd033
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-10-19 09:45:49 +0500

    logic19.py

--- logic19.py
+++ logic19.py
@@ -8,4 +8,9 @@ def main(x):
     Returns:
         bool: answer
     """
-    return
+    if 9<x<1000 and x//100==0:
+        return x%100//10+x%10*10==x 
+    else:
+        return x%10*100+x%100//10*10+x//100==x
+        
+print(main(22))
